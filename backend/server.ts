ed1f6f7ddbb3a9d93aecb5891f9e074da01f4f3c
viren-01 <vishwakarmavirendra74@gmail.com>
2024-05-25 02:22:26 +0530

    Adding self pinging for render

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import http from 'http';
 import app from './app';
+import axios from 'axios'
 
 class Server {
     private server: http.Server
@@ -15,6 +16,15 @@ class Server {
         try {
             this.server.listen(this.port, () => {
                 console.log("Server started at port 8008")
+                if (process.env.ENV === 'PROD') {
+                    //self ping for render
+                    setInterval(() => {
+                        let url = `${process.env.HOST}/health`
+                        axios.get(url).then((res) => {
+                            console.log("Self Request Made...")
+                        })
+                    }, 840000)
+                }
             })   
         } catch (error) {
             console.log("error in starting server")
