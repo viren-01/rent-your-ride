fed19eb20188c8b02224d559c28358abe328de33
viren-01 <vishwakarmavirendra74@gmail.com>
2024-05-25 02:20:52 +0530

    Adding prod script

--- backend/server.ts
+++ backend/server.ts
@@ -3,15 +3,17 @@ import app from './app';
 
 class Server {
     private server: http.Server
+    private port: string
 
     constructor() {
         this.server = http.createServer(app)
+        this.port = process.env.PORT || '8008'
         this.start()
     }
 
     private start() {
         try {
-            this.server.listen(8008, () => {
+            this.server.listen(this.port, () => {
                 console.log("Server started at port 8008")
             })   
         } catch (error) {
