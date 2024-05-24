a191c3fb153485309e18e037ec28dbb6c0406228
viren-01 <vishwakarmavirendra74@gmail.com>
2024-05-25 03:00:25 +0530

    editing project title & icon

--- frontend/src/config/constant.js
+++ frontend/src/config/constant.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://localhost:8008/api'//process.env.REACT_APP_BACKEND_URL = "/api"
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL + "/api" //'http://localhost:8008/api'//
