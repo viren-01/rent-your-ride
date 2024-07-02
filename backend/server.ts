89e54c59d14eb876095ccd3ca7745108024806e4
viren-01 <vishwakarmavirendra74@gmail.com>
2024-07-02 23:11:48 +0530

    removing auto ping service

--- backend/server.ts
+++ backend/server.ts
@@ -18,16 +18,16 @@ class Server {
                 console.log("Server started at port 8008")
                 if (process.env.ENV === 'PROD') {
                     //self ping for render
-                    const curDay = new Date().getUTCDay()
+                    // const curDay = new Date().getUTCDay()
                     //if day is not saturday & sunday
-                    if(![6, 7].includes(curDay)) {
-                        setInterval(() => {
-                            let url = `${process.env.HOST}/health`
-                            axios.get(url).then((res) => {
-                                console.log("Self Request Made...")
-                            })
-                        }, 840000)
-                    }
+                    // if(![6, 7].includes(curDay)) {
+                    //     setInterval(() => {
+                    //         let url = `${process.env.HOST}/health`
+                    //         axios.get(url).then((res) => {
+                    //             console.log("Self Request Made...")
+                    //         })
+                    //     }, 840000)
+                    // }
                 }
             })   
         } catch (error) {
